6d1bb3b636c2d1cd3ff10e76f837704d6dc337e6
Cody Lejang <codylejang@gmail.com>
2025-05-29 03:10:03 -0700

    added show all functionality, made tabbar modular, added filtering / sorting skeleton

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,11 @@
 export type RootStackParamList = {
-  Login: { setIsLoggedIn: (loggedIn: boolean) => void };
-  Signup: { setIsLoggedIn: (loggedIn: boolean) => void };
-  MainApp: undefined;
+  Login:   { setIsLoggedIn: (loggedIn: boolean) => void };
+  Signup:  { setIsLoggedIn: (loggedIn: boolean) => void };
+  Home:    undefined;
+  Upload:  undefined;
+  Tops:    undefined;
+  Bottoms: undefined;
+  Shoes:   undefined;
+  Outfits: undefined;
   CreateOutfit: undefined;
 };
