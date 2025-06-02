876240f9e794a75779df8d6929a2f5f7ca18b75c
Cody Lejang <codylejang@gmail.com>
2025-06-02 11:11:17 -0700

    test

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = 'https://1adb-2607-f010-2e9-44-4d38-b31f-f0e1-b5dd.ngrok-free.app';
+export const BACKEND_URL = 'https://6b23-2607-f010-2e9-2-5523-b673-cd32-d4e2.ngrok-free.app';
