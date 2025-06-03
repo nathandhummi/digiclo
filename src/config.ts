2630217e8ca9e249d773a127ed8c620739e40834
Cody Lejang <codylejang@gmail.com>
2025-06-02 19:26:03 -0700

    back button, search for tags

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = 'https://6b23-2607-f010-2e9-2-5523-b673-cd32-d4e2.ngrok-free.app';
+export const BACKEND_URL = 'https://0d2b-2607-f010-2e9-1-893e-5c1b-a641-797a.ngrok-free.app';
